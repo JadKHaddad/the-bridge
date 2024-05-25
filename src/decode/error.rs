dc632dd5c85624992a1e50dd4ac54f5d13f84bbb
JadKHaddad <jadkhaddad@gmail.com>
2024-05-25 09:25:35 +0200

    added: std feature, implemented: std::error::Error for EncodeError and DecodeError
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src/decode/error.rs
+++ src/decode/error.rs
@@ -5,3 +5,19 @@ pub enum DecodeError<E> {
     ReadZero,
     BufferIsFull,
 }
+
+#[cfg(feature = "std")]
+const _: () = {
+    impl<E: std::fmt::Display> std::fmt::Display for DecodeError<E> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Self::Io(error) => write!(f, "IO error: {}", error),
+                Self::Decode(error) => write!(f, "Decode error: {}", error),
+                Self::ReadZero => write!(f, "Read zero bytes"),
+                Self::BufferIsFull => write!(f, "Buffer is full"),
+            }
+        }
+    }
+
+    impl<E: std::error::Error> std::error::Error for DecodeError<E> {}
+};
--- src/encode/error.rs
+++ src/encode/error.rs
@@ -5,3 +5,19 @@ pub enum EncodeError<E> {
     BufferTooShort,
     MessageTooLarge,
 }
+
+#[cfg(feature = "std")]
+const _: () = {
+    impl<E: std::fmt::Display> std::fmt::Display for EncodeError<E> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Self::Io(error) => write!(f, "IO error: {}", error),
+                Self::Encode(error) => write!(f, "Encode error: {}", error),
+                Self::BufferTooShort => write!(f, "Buffer is too short"),
+                Self::MessageTooLarge => write!(f, "Message is too large"),
+            }
+        }
+    }
+
+    impl<E: std::error::Error> std::error::Error for EncodeError<E> {}
+};
